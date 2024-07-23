d95ad1dc17c643d85d774191b7d30336ae8d6945
Thibault R <44661431+R-Thibault@users.noreply.github.com>
2024-07-23 19:46:27 +0200

    HEAD- 133-ressources---ajout-du-lien-fichier-sur-la-ressource- 133-ressources---ajout-du-lien-fichier-sur-la-ressource- 133-ressources---ajout-du-lien-fichier-sur-la-ressource- Wip display ressourcesCard on form's side

--- frontend/src/components/molecules/ressourceCard.tsx
+++ frontend/src/components/molecules/ressourceCard.tsx
@@ -113,6 +113,29 @@ export default function RessourceCard(
         <div className="card-body pb-5">
           <div className="d-flex gap-1"></div>
           <h5 className="card-title pt-2 title">{ressource.title}</h5>
+          {ressource.link_id && (
+            <a
+              href={ressource.link_id.url}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <i className="bi bi-link"></i>
+            </a>
+          )}
+          {ressource.file_id && (
+            <a
+              href={`http://localhost:4000/download/${ressource.file_id.path.replace(
+                "/app/upload/ressources/",
+                ""
+              )}`}
+              download={ressource.file_id.title}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <i className="bi bi-file-earmark-arrow-down"></i>{" "}
+              {ressource.file_id.title}
+            </a>
+          )}
           <p className="card-text description">{ressource.description}</p>
         </div>
         {dataUser?.item?.id === ressource?.created_by_user?.id && (
