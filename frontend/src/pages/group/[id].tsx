c4df13913f5f0564854a26f90fe951b26c4f643d
Mathieu HERY <mathieu1.hery@gmail.com>
2024-07-23 17:26:35 +0200

    HEAD- clean-update-desktop- develop- clean project and update desktop layout

--- frontend/src/pages/group/[id].tsx
+++ frontend/src/pages/group/[id].tsx
@@ -17,9 +17,6 @@ import { InView } from "react-intersection-observer";
 import SharingGroupForm from "@/components/organisms/sharingGroupForm";
 import ModalComponent from "@/components/organisms//modal";
 import CreateRessourcesForm from "@/components/organisms/createRessourcesForm";
-import { GET_ALL_TAGS_FROM_ONE_USER } from "@/requests/tags";
-import TagsDisplay from "@/components/organisms/tagsDisplay";
-import { TagType } from "@/types/extra.types";
 import ChatDisplay from "@/components/organisms/chatDisplay";
 import { MY_PROFILE } from "@/requests/user";
 import { UserType } from "@/types/user.types";
@@ -40,7 +37,6 @@ export default function GroupDashboard(): React.ReactNode {
   const [modalRessourceVisible, setmodalRessourceVisible] =
       useState<boolean>(false);
   const [chatVisible, setChatVisible] = useState<boolean>(false);
-  const [selectedTags, setSelectedTags] = useState<TagType[]>([]);
   const [, setSkip] = useState<number>(0);
   const [take] = useState<number>(10);
   const [titleSort, setTitleSort] = useState<string>("");
@@ -68,9 +64,6 @@ export default function GroupDashboard(): React.ReactNode {
       id: groupId,
     },
   });
-  const { data: dataTags } = useQuery<{ items: TagType[] }>(
-      GET_ALL_TAGS_FROM_ONE_USER
-  );
   const {
     data: dataRessources,
     error: errorRessources,
@@ -159,20 +152,20 @@ export default function GroupDashboard(): React.ReactNode {
     },
     refetchQueries: [GET_MY_GROUPS],
   });
-  function handleSelectTag(tag: TagType) {
-    if (selectedTags.find((item) => item === tag)) {
-      const newTagArray = selectedTags.filter((item) => item !== tag);
-      setSelectedTags(newTagArray);
-    } else {
-      setSelectedTags([...selectedTags, tag]);
-    }
-  }
 
   async function handleGroupQuit(isCreator: boolean) {
-    if (isCreator) {
-      await groupDelete();
-    } else {
-      await memberDelete();
+    if (
+      confirm(
+        isCreator
+          ? "Êtes-vous sûr de vouloir supprimer ce groupe ?"
+          : "Êtes-vous sûr de vouloir quitter ce groupe ?"
+      )
+    ) {
+      if (isCreator) {
+        await groupDelete();
+      } else {
+        await memberDelete();
+      }
     }
   }
   useEffect(() => {
@@ -197,153 +190,136 @@ export default function GroupDashboard(): React.ReactNode {
   }, [titleSort]);
 
   return (
-      <Layout title={"Dashboard Groupe"}>
-        <div className="ressources_main_container">
-          {dataGroup ? (
-              <div className="d-flex justify-content-between align-items-center w-100">
-                <div className="d-flex">
-                  <h1>{dataGroup.item.name}</h1>
-                  <button
-                      className="btn_primary menu_button_add_group mx-4"
-                      onClick={() => handleInviteMemberModal(true)}
-                  >
-                    <i className="bi bi-plus-circle" />
-                    <span>Partager</span>
-                  </button>
-                </div>
-                <div className="d-flex justify-content-start align-items-center search_input_container">
-                  <i className="bi bi-search"></i>
-                  <input
-                      type="text"
-                      placeholder="Rechercher par titre"
-                      onChange={(e) => setSearchTitle(e.target.value)}
-                  />
-                </div>
-                <button
-                    className="btn_primary mx-4 message_btn"
-                    onClick={() => handleChatVisible(true)}
-                >
-                  <i className="bi bi-plus-circle" />
-                  <span>Messages</span>
-                </button>
-              </div>
-          ) : (
-              <>
-                <h2>Group Not Found</h2>
-              </>
-          )}
-          <div
-              className={`d-flex flex-row justify-content-between align-items-center mt-2 w-100`}
-          >
-            <div className="add_ressources_button">
-              <h2>Ressources</h2>
+    <Layout title={"Dashboard Groupe"}>
+      <div className="ressources_main_container">
+        {dataGroup ? (
+          <div className="d-flex justify-content-between align-items-center w-100">
+            <div className="d-flex">
+              <h1>{dataGroup.item.name}</h1>
               <button
-                  className="btn_rounded btn_add_ressources"
-                  onClick={() => handleResourceModal(true)}
+                className="btn_rounded btn_rounded_actions"
+                onClick={() => handleInviteMemberModal(true)}
               >
-                <i className="bi bi-plus-circle" />
+                <i className="bi bi-envelope-arrow-up" />
               </button>
-            </div>
-            <div>
               <button
-                  className="btn_rounded btn_quit_group"
-                  onClick={() =>
-                      handleGroupQuit(
-                          dataUser?.item?.id === dataGroup?.item?.created_by_user?.id
-                      )
-                  }
+                className="btn_rounded btn_rounded_actions btn_message"
+                onClick={() => handleChatVisible(true)}
               >
-              <span>
-                {dataUser?.item?.id === dataGroup?.item?.created_by_user?.id
-                    ? `Supprimer le groupe`
-                    : `Quitter le groupe`}
-              </span>
+                <i className="bi bi-chat" />
+              </button>
+              <button
+                className="btn_rounded btn_rounded_actions btn_quit_group "
+                onClick={() =>
+                  handleGroupQuit(
+                    dataUser?.item?.id === dataGroup?.item?.created_by_user?.id
+                  )
+                }
+              >
+                <i className="bi bi-x-lg" />
               </button>
               {deleteGroupError && (
-                  <span className="text-danger">{deleteGroupError}</span>
+                <span className="text-danger">{deleteGroupError}</span>
               )}
             </div>
-          </div>
-          <div
-              className={`d-flex flex-row align-items-center mt-2 w-100 ${
-                  dataTags?.items ? "justify-content-between" : "justify-content-end"
-              }`}
-          >
-            {dataTags?.items && (
-                <TagsDisplay
-                    tags={dataTags?.items}
-                    selectedTags={selectedTags}
-                    onSelectTag={(tag: TagType) => handleSelectTag(tag)}
-                />
-            )}
-            <div className="d-flex flex-row align-items-center justify-content-center gap-2 sort_buttons_container ">
-              <span>Trier par:</span>
-              <button
-                  className="btn_sort"
-                  onClick={() =>
-                      titleSort === "DESC" || titleSort === ""
-                          ? setTitleSort("ASC")
-                          : setTitleSort("DESC")
-                  }
-              >
-                <i id="title-sort" className={titleSortClass}></i>
-                Titre
-              </button>
-              <button
-                  className="btn_sort"
-                  onClick={() =>
-                      dateSort === "DESC" || dateSort === ""
-                          ? setDateSort("ASC")
-                          : setDateSort("DESC")
-                  }
-              >
-                <i id="date-sort" className={dateSortClass}></i>
-                Date
-              </button>
+            <div className="d-flex justify-content-start align-items-center search_input_container">
+              <i className="bi bi-search"></i>
+              <input
+                type="text"
+                placeholder="Rechercher par titre"
+                onChange={(e) => setSearchTitle(e.target.value)}
+              />
             </div>
           </div>
-          <div className="col-md-12">
-            <div className="row">
-              <div>
-                {loadingRessources && (
-                    <Spinner animation="border" role="status">
-                      <span className="visually-hidden">Loading...</span>
-                    </Spinner>
-                )}
-                {errorRessources && <p>{errorRessources.message}</p>}
-                {dataRessources && (
-                    <CardsDisplay ressources={dataRessources?.items} />
-                )}
-                <InView onChange={handleFetchMore} threshold={0.5}>
-                  <div className="spinner"></div>
-                </InView>
-              </div>
-            </div>
+        ) : (
+          <>
+            <h2>Group Not Found</h2>
+          </>
+        )}
+        <div
+          className={`d-flex flex-row justify-content-between align-items-center mt-2 w-100`}
+        >
+          <div className="add_ressources_button">
+            <button
+              className="btn_primary message_btn"
+              onClick={() => handleResourceModal(true)}
+            >
+              <i className="bi bi-plus-circle" />
+              <span>Ajouter une ressource</span>
+            </button>
           </div>
-          <ModalComponent
-              opened={modalRessourceVisible}
-              openModal={handleResourceModal}
-          >
-            <CreateRessourcesForm
-                onClose={handleResourceModal}
-                groupId={groupId}
-            />
-          </ModalComponent>
-          <ModalComponent
-              opened={modalInviteMemberVisible}
-              openModal={handleInviteMemberModal}
-          >
-            <SharingGroupForm
-                groupId={groupId}
-                onClose={handleInviteMemberModal}
-            />
-          </ModalComponent>
-          <ChatDisplay
-              groupId={groupId}
-              opened={chatVisible}
-              handleChatDisplay={handleChatVisible}
-          />
+          <div className="d-flex flex-row align-items-center justify-content-center gap-2 sort_buttons_container ">
+            <span>Trier par:</span>
+            <button
+              className="btn_sort"
+              onClick={() =>
+                titleSort === "DESC" || titleSort === ""
+                  ? setTitleSort("ASC")
+                  : setTitleSort("DESC")
+              }
+            >
+              <i id="title-sort" className={titleSortClass}></i>
+              Titre
+            </button>
+            <button
+              className="btn_sort"
+              onClick={() =>
+                dateSort === "DESC" || dateSort === ""
+                  ? setDateSort("ASC")
+                  : setDateSort("DESC")
+              }
+            >
+              <i id="date-sort" className={dateSortClass}></i>
+              Date
+            </button>
+          </div>
         </div>
-      </Layout>
+        <div className="col-md-12">
+          <div className="row">
+            <div>
+              {loadingRessources && (
+                <div className="loader_container">
+                  <div className="d-inline-flex">
+                    <Spinner className="spinner_purple" animation="grow" />
+                    <Spinner className="spinner_blue" animation="grow" />
+                    <Spinner className="spinner_red" animation="grow" />
+                  </div>
+                </div>
+              )}
+              {errorRessources && <p>{errorRessources.message}</p>}
+              {dataRessources && (
+                <CardsDisplay ressources={dataRessources?.items} />
+              )}
+              <InView onChange={handleFetchMore} threshold={0.5}>
+                <div className="spinner"></div>
+              </InView>
+            </div>
+          </div>
+        </div>
+        <ModalComponent
+          opened={modalRessourceVisible}
+          openModal={handleResourceModal}
+        >
+          <CreateRessourcesForm
+            onClose={handleResourceModal}
+            groupId={groupId}
+          />
+        </ModalComponent>
+        <ModalComponent
+          opened={modalInviteMemberVisible}
+          openModal={handleInviteMemberModal}
+        >
+          <SharingGroupForm
+            groupId={groupId}
+            onClose={handleInviteMemberModal}
+          />
+        </ModalComponent>
+        <ChatDisplay
+          groupId={groupId}
+          opened={chatVisible}
+          handleChatDisplay={handleChatVisible}
+        />
+      </div>
+    </Layout>
   );
 }
